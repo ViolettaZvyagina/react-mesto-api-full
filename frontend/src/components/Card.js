1fdf26968f5b1b8739b8b018a493c750da8b518b
Violetta Zvyagina <zvy4@yandex.ru>
2022-09-28 17:04:25 +0300

    Проект 15

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,12 +4,12 @@ import {CurrentUserContext} from "../contexts/CurrentUserContext";
 function Card({card, onCardClick, onCardLike, onCardDelete}) {
 
   const currentUser = React.useContext(CurrentUserContext);
-  const isOwn = card.owner._id === currentUser._id;
+  const isOwn = card.owner === currentUser._id;
   const cardDeleteButtonClassName = (
     `card__delete-button ${isOwn ? 'card__delete-button_active' : ''}`
   ); 
 
-  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const isLiked = card.likes.some(i => i === currentUser._id);
   const cardLikeButtonClassName = (
     `card__like-button ${isLiked ? 'card__like-button_active' : ''}`);
   
